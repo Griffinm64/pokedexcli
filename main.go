eb056e6a462c114a4dd116576dfd3c4df64ca75c
griffinm64 <61828980+Griffinm64@users.noreply.github.com>
2026-01-05 05:46:26 -0500

    Added input cleaning and tests

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"strings"
+)
 
-func main() {
-	fmt.Println("Hello, World!")
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
 }
