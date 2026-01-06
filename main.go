d80fa3821865e54892b07891e8d0e13cd9bf2c45
griffinm64 <61828980+Griffinm64@users.noreply.github.com>
2026-01-06 05:38:37 -0500

    Added basic REPL to clean text

--- main.go
+++ main.go
@@ -2,7 +2,23 @@ package main
 
 import (
 	"strings"
+	"bufio"
+	"os"
+	"fmt"
 )
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		 if scanner.Scan() != false {
+			dirtyText := scanner.Text()
+			cleandedtext := cleanInput(dirtyText)
+			fmt.Println("Your command was: " + cleandedtext[0])
+		 }
+
+	}
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
